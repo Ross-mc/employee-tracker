510d2ab21560c4204009a698c1d408901ceac754
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-01-28 15:02:15 +0000

    Feat: can now display all employees by their manager

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const displayEmployees = require('./lib/query');
+const { displayEmployees, getManagers } = require('./lib/query');
 
 const initialPrompt = {
     type: 'list',
@@ -13,7 +13,7 @@ const initialPrompt = {
         'Update existing employee',
         'Delete information',
         'Exit'
-    ]
+    ] //choices can be improved -- read inquirer docs
 };
 
 const viewEmployeePrompts = {
@@ -29,31 +29,44 @@ const viewEmployeePrompts = {
 };
 
 const managerSelectionPrompts = {
-    type: 'input',
+    type: 'list',
     name: 'manager',
-    message: 'Please enter the name of the manager you wish to search for'
+    message: 'Which manager would you like to search by?',
+    choices: []
 }
 
 const userViewEmployees = async () => {
-    let response;
+    let response = '';
     await inquirer
     .prompt(viewEmployeePrompts)
     .then(res => {
         response = res.option
     });
-    //to do: add hashmap logic, further program flow through the various queries
-
-    try {
-        if (response === 'View all employees'){
-            console.log(await displayEmployees('all'));
-            
-        }
-        main();
-    } catch(e){
-        console.log(e)
+    //to do: add hashmap logic, further program flow through the various queries ***improve choices
+    if (response === 'View all employees') {
+        try {
+            console.log(await displayEmployees('all')); 
+            main();
+        } catch(e){
+            console.log(e)
+        };
     };
 
-
+    if (response === viewEmployeePrompts.choices[1]){
+        const managers = await getManagers();
+        managerSelectionPrompts.choices = managers.map(manager => `${manager.first_name} ${manager.last_name}`);
+        inquirer
+        .prompt(managerSelectionPrompts)
+        .then(async res => {
+            const firstName = res.manager.split(' ')[0];
+            const lastName = res.manager.split(' ')[1];
+            const managerToFind = managers.filter(manager => manager.first_name === firstName && manager.last_name === lastName);
+            const { id } = managerToFind[0];
+            const result = await displayEmployees('manager', id);
+            console.log(result)
+            main();
+        })
+    }
 
 }
 
